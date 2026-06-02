8d9afee16fafc9b64fe265213ab9f8e73a8ce719
gunasow <sowbakiya8352@gmail.com>
2026-06-02 14:41:17 +0530

    day1 assignment

---
+++ JavaScript/01-06-2026/variables.js
@@ -0,0 +1,23 @@
+let userName = ""
+let accountBalance = 0
+let profitMargin = 100
+const courseList = [101,102,103]
+const skuID = "SKU002"
+const countryCode = "IN"
+const authenticationToken = "ecim39mcwk38"
+const workerCount = 10
+
+
+function checkParanthesis (sequence) {
+    let stack = []
+    for (let i=0 ; i<length(sequence); i++) {
+        // code for checkParanthesis
+
+    }
+    
+    return len(stack)!=0 ? false : true
+
+}
+
+console.log(checkParanthesis("[]["))
+console.log(stack)
